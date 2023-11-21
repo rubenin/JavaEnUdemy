a51b41c51c8fd66e6cdc1905d48d7d69973caf12
Ruben Gonzalez <rubengonzalez@MacBook-Pro.local>
2023-11-21 18:24:30 +0100

    Comenzando el curso de AlejandroMiguel

---
+++ Seccion1/HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Seccion1;
+
+/**
+ *
+ * @author rubengonzalez
+ */
+public class HolaMundo {
+    
+    /**
+     * @param args the command line
+     */
+    
+    public static void main(String[] args){
+        System.out.print("HolaMundo");
+        
+    }
+}
